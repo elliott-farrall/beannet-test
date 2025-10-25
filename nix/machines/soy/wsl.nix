846da1ed3a8b0254e73b8e5a15de212ead270f7d
elliott-farrall <dev@elliott-farrall.phd>
2025-10-25 09:26:52 +0100

    Disable host generation in WSL configuration for soy

--- nix/machines/soy/wsl.nix
+++ nix/machines/soy/wsl.nix
@@ -4,5 +4,6 @@
   flake.clan.machines."soy" = { lib, ... }: {
     wsl.enable = true;
     networking.networkmanager.enable = lib.mkForce false;
+    wsl.wslConf.network.generateHosts = false;
   };
 }
