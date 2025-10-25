009fb67fb7bb6ee5ad607679e6b29e413afa8697
elliott-farrall <dev@elliott-farrall.phd>
2025-10-25 10:01:44 +0100

    Revert previous

--- nix/machines/soy/wsl.nix
+++ nix/machines/soy/wsl.nix
@@ -4,6 +4,5 @@
   flake.clan.machines."soy" = { lib, ... }: {
     wsl.enable = true;
     networking.networkmanager.enable = lib.mkForce false;
-    wsl.wslConf.network.generateHosts = false;
   };
 }
