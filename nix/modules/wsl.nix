a3f23b08a964468d0d120d497f9aa7cad86c54a2
elliott-farrall <dev@elliott-farrall.phd>
2026-03-21 23:06:50 +0000

    Disable DHCP for WSL eth0 interface

--- nix/modules/wsl.nix
+++ nix/modules/wsl.nix
@@ -8,7 +8,7 @@
       # wsl.wslConf.network.generateResolvConf = false;
       # networking.nameservers = [ "1.1.1.1" "9.9.9.9" ];
       networking.interfaces."eth0" = {
-        useDHCP = true;
+        useDHCP = false;
         mtu = 1400;
       };
 
