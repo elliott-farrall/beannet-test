e68131d74b3fbb83c898892b75e168434234ac24
elliott-farrall <dev@elliott-farrall.phd>
2026-03-22 11:48:06 +0000

    Refactor WSL network configuration for systemd

--- nix/modules/wsl.nix
+++ nix/modules/wsl.nix
@@ -7,9 +7,13 @@
     config = lib.mkIf config.wsl.enable {
       # wsl.wslConf.network.generateResolvConf = false;
       # networking.nameservers = [ "1.1.1.1" "9.9.9.9" ];
-      systemd.network.networks."10-eth0" = {
-        matchConfig.Name = "eth0";
-        linkConfig.MTUBytes = "1400";
+      systemd.network = {
+        enable = true;
+
+        networks."10-eth0" = {
+          matchConfig.Name = "eth0";
+          linkConfig.MTUBytes = "1400";
+        };
       };
 
       programs.nix-ld.enable = true; # Allows vscode remote access
